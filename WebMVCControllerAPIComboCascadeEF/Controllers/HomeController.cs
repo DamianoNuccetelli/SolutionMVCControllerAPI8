2d4185d9d8ec117f52dcb110e2611e098d52c7e4
DamianoNuccetelli <damianonuccetelli1997@gmail.com>
2024-03-01 12:43:47 +0100

    aggiunto controller

---
+++ WebMVCControllerAPIComboCascadeEF/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVCControllerAPIComboCascadeEF.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
